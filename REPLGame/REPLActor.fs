907276f0d11f04e033a7820fe788147817bd63ab
Anthony Brown <bruinbrown@gmail.com>
2013-10-21 10:56:51 +0100

    Added the basics for the game

---
+++ REPLGame/REPLActor.fs
@@ -0,0 +1,15 @@
+﻿module REPLActor
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Content
+open Microsoft.Xna.Framework.Graphics
+
+type Actor =
+    {
+        Texture : Texture2D;
+        Position : Vector2
+    }
+
+let CreateActor (content:ContentManager) fileName position =
+    let t = content.Load<Texture2D> fileName
+    { Texture = t; Position = position }
---
+++ REPLGame/REPLGame.fs
@@ -0,0 +1,36 @@
+﻿module REPLGame
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open REPLActor
+
+type REPLGame () as this = 
+    inherit Game ()
+
+    do this.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    let mutable (actors:Actor list) = []
+
+    let DrawActor (sb:SpriteBatch) actor =
+        do sb.Draw(actor.Texture, actor.Position, Color.White)
+
+    member this.Actors
+        with get() = actors
+        and set(v) = actors <- v
+
+    override this.Initialize() =
+        do spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+        do base.Initialize ()
+        ()
+
+    override this.Draw gameTime = 
+        do this.GraphicsDevice.Clear Color.CornflowerBlue
+        let DrawActor' = DrawActor spriteBatch
+        do spriteBatch.Begin()
+        actors
+        |> List.iter DrawActor'
+        do spriteBatch.End()
+        ()
+
